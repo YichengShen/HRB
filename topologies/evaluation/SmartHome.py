7d60a216eb7895722b1c6d5a56aeba16abac6b16
YichengShen <jsszsyc@hotmail.com>
2021-10-23 13:40:02 +0800

    fix bandwidth limit

--- topologies/evaluation/SmartHome.py
+++ topologies/evaluation/SmartHome.py
@@ -1,6 +1,9 @@
 from mininet.topo import Topo
+from mininet.link import TCLink
 from mininet.util import irange
 
+
+BW_LIMITED = 5
 
 class SmartHome(Topo):
     """
@@ -8,14 +11,20 @@ class SmartHome(Topo):
     Added limitations on bandwidth, delay, jitter, and loss.
     """
 
-    def build(self, k=40, bw=50000000, delay='10ms', jitter='3ms', loss=2):
+    def build(self, k=40, bw=5, delay='10ms', jitter='3ms', loss=2):
         "k: number of hosts"
         self.k = k
         switch = self.addSwitch('s1')
         for h in irange(1, k):
             host = self.addHost('h%s' % h)
+            # Set 1/3 hosts to have highly limited bandwidth
+            if h % 3 == 0:
+                link_bw = BW_LIMITED
+            else:
+                link_bw = bw
             self.addLink(host, switch,
-                         bw=bw,
+                         cls=TCLink,
+                         bw=link_bw,
                          delay=delay,
                          jitter=jitter,
                          loss=loss)
