b7be3005ef8e8c49c251625664e9ed8f813937a8
YichengShen <jsszsyc@hotmail.com>
2021-10-23 13:43:57 +0800

    fix bw limit for payment system

--- topologies/evaluation/PaymentSystem.py
+++ topologies/evaluation/PaymentSystem.py
@@ -1,14 +1,16 @@
 from mininet.topo import Topo
+from mininet.link import TCLink
 
 
 class PaymentSystem(Topo):
 
-    def build(self, e=10, bw=10000000, delay='50ms', jitter='25ms', loss=0):
+    def build(self, e=10, bw=10, delay='50ms', jitter='25ms', loss=0):
         core = self.addSwitch('s1')
 
         for i in range(1, e+1):
             edge = self.addSwitch('s%s' % str(i+1))
             self.addLink(core, edge,
+                         cls=TCLink,
                          bw=bw,
                          delay=delay,
                          jitter=jitter,
@@ -16,6 +18,7 @@ class PaymentSystem(Topo):
                          )
             host =self.addHost('h%s' % i)
             self.addLink(edge, host,
+                         cls=TCLink,
                          bw=bw,
                          delay=delay,
                          jitter=jitter,
